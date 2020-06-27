ab1bc8725f14e7429f670b3eff7b01314ea560e7
vegirevathi <vegirevathi@gmail.com>
2020-06-27 08:25:14 +0530

    ADDED: Method to add a new person in address book

--- src/main/java/com/bridgelabz/AddressBookMain.java
+++ src/main/java/com/bridgelabz/AddressBookMain.java
@@ -1,7 +1,30 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to Advanced Address Book");
+
+         Scanner input=new Scanner(System.in);
+
+        public void addPerson()
+        {
+            System.out.println("enter first name");
+            String firstName = input.nextLine();
+            System.out.println("enter last name");
+            String lastName = input.nextLine();
+            System.out.println("enter address");
+            String address = input.nextLine();
+            System.out.println("enter city");
+            String city = input.nextLine();
+            System.out.println("enter state");
+            String state = input.nextLine();
+            System.out.println("enter zip");
+            String zip = input.nextLine();
+            System.out.println("enter phone number");
+            String phoneNumber = input.nextLine();
+            Person person1 = new Person(firstName, lastName, address, city, state, zip, phoneNumber);
+        }
     }
 }

2b50753ab570cbf9f162e4b219738954b8eff39d
vegirevathi <vegirevathi@gmail.com>
2020-06-27 08:32:33 +0530

    ADDED: Method to edit contact details

--- src/main/java/com/bridgelabz/AddressBookMain.java
+++ src/main/java/com/bridgelabz/AddressBookMain.java
@@ -1,14 +1,16 @@
 package com.bridgelabz;
 
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class AddressBookMain {
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to Advanced Address Book");
 
-         Scanner input=new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+        private final LinkedList<Person> personsList;
 
-        public void addPerson()
+        public void addPerson ()
         {
             System.out.println("enter first name");
             String firstName = input.nextLine();
@@ -26,5 +28,68 @@ public class AddressBookMain {
             String phoneNumber = input.nextLine();
             Person person1 = new Person(firstName, lastName, address, city, state, zip, phoneNumber);
         }
+
+        public void editperson ()
+        {
+            System.out.println("enter first name of person ");
+            String firstName = input.nextLine();
+            Person personToEdit = getObjectWithName(firstName);
+            boolean quit = false;
+            do {
+                System.out.println("enter 1 for editing address");
+                System.out.println("enter 2 for editing city");
+                System.out.println("enter 3 for editing state");
+                System.out.println("enter 4 for editing zip");
+                System.out.println("enter 5 for editing phone number");
+                System.out.println("enter 6 to quit");
+                int choice = input.nextInt();
+                input.nextLine();
+                switch (choice) {
+                    case 1:
+                        System.out.println("enter new address");
+                        String address = input.nextLine();
+                        personToEdit.setAddress(address);
+                        break;
+                    case 2:
+                        System.out.println("enter new city");
+                        String city = input.nextLine();
+                        personToEdit.setCity(city);
+                        break;
+                    case 3:
+                        System.out.println("enter new state");
+                        String state = input.nextLine();
+                        personToEdit.setState(state);
+                        break;
+                    case 4:
+                        System.out.println("enter new zip");
+                        String zip = input.nextLine();
+                        personToEdit.setZip(zip);
+                        break;
+                    case 5:
+                        System.out.println("enter new phone number");
+                        String phoneNumber = input.nextLine();
+                        personToEdit.setPhoneNumber(phoneNumber);
+                        break;
+                    case 6:
+                        quit = true;
+                        break;
+                }
+
+            } while (!quit);
+
+        }
+
+        public Person getObjectWithName(String firstName)
+        {
+            for(Person person:personsList)
+            {
+                if(person.getFirstName().equals(firstName))
+                {
+                    return person;
+                }
+            }
+            return new Person("","","","","","","");
+        }
+
     }
 }

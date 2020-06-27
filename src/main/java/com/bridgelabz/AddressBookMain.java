da95a57270ed52fe781d24b48482a201e4569ca3
vegirevathi <vegirevathi@gmail.com>
2020-06-27 08:57:03 +0530

    ADDED: Sorting method to sort Contacts by last name

--- src/main/java/com/bridgelabz/AddressBookMain.java
+++ src/main/java/com/bridgelabz/AddressBookMain.java
@@ -1,112 +1,149 @@
 package com.bridgelabz;
 
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Scanner;
 
 public class AddressBookMain {
+    static Scanner input = new Scanner(System.in);
+    LinkedList<Person> personsList;
+
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to Advanced Address Book");
-
-        Scanner input = new Scanner(System.in);
-        private final LinkedList<Person> personsList;
-
-        public void addPerson ()
-        {
-            System.out.println("enter first name");
-            String firstName = input.nextLine();
-            System.out.println("enter last name");
-            String lastName = input.nextLine();
-            System.out.println("enter address");
-            String address = input.nextLine();
-            System.out.println("enter city");
-            String city = input.nextLine();
-            System.out.println("enter state");
-            String state = input.nextLine();
-            System.out.println("enter zip");
-            String zip = input.nextLine();
-            System.out.println("enter phone number");
-            String phoneNumber = input.nextLine();
-            Person person1 = new Person(firstName, lastName, address, city, state, zip, phoneNumber);
-            personsList.add(person1);
-            Person duplicate=getObjectWithName(firstName);
-            if(person1.equals(duplicate))
-            {
-                System.out.println("there already exists person  with same name in address book");
-                return;
+        boolean quit=false;
+        AddressBookMain addressBook=new AddressBookMain();
+        do{
+            System.out.println("enter 1 for adding person to address book");
+            System.out.println("enter 2 for editing person");
+            System.out.println("enter 3 for deleting a person");
+            System.out.println("enter 4 to display Address book");
+            System.out.println("ener 5 to sort address book by name");
+            System.out.println("ener 6 to sort address book by city");
+            System.out.println("ener 7 to sort address book by state");
+            System.out.println("ener 8 to sort address book by zip");
+            System.out.println("ener 9 to view person by city");
+            System.out.println("ener 10 to view person by state");
+            System.out.println("enter 11 to quit");
+            int option=input.nextInt();
+            input.nextLine();
+            switch (option) {
+                case 1:
+                    addressBook.addPerson();
+                    break;
+                case 2:
+                    addressBook.displayAddressBook();
+                    addressBook.editperson();
+                    break;
+                case 3:
+                    addressBook.displayAddressBook();
+                    addressBook.deletePerson();
+                    break;
+                case 4:
+                    addressBook.displayAddressBook();
+                    break;
+                case 5:
+                    Collections.sort(addressBook.personsList);
+                    break;
+                case 6:
+                    quit=true;
+                    break;
             }
-            personsList.add(person1);
-            addValues(cityMap,city,person1);
-            addValues(stateMap,state,person1);
+        }while (!quit);
+    }
+
+    private void displayAddressBook() {
+        for(Person person:personsList) {
+            System.out.println(person);
         }
+    }
 
-        public void editperson ()
-        {
-            System.out.println("enter first name of person ");
-            String firstName = input.nextLine();
-            Person personToEdit = getObjectWithName(firstName);
-            boolean quit = false;
-            do {
-                System.out.println("enter 1 for editing address");
-                System.out.println("enter 2 for editing city");
-                System.out.println("enter 3 for editing state");
-                System.out.println("enter 4 for editing zip");
-                System.out.println("enter 5 for editing phone number");
-                System.out.println("enter 6 to quit");
-                int choice = input.nextInt();
-                input.nextLine();
-                switch (choice) {
-                    case 1:
-                        System.out.println("enter new address");
-                        String address = input.nextLine();
-                        personToEdit.setAddress(address);
-                        break;
-                    case 2:
-                        System.out.println("enter new city");
-                        String city = input.nextLine();
-                        personToEdit.setCity(city);
-                        break;
-                    case 3:
-                        System.out.println("enter new state");
-                        String state = input.nextLine();
-                        personToEdit.setState(state);
-                        break;
-                    case 4:
-                        System.out.println("enter new zip");
-                        String zip = input.nextLine();
-                        personToEdit.setZip(zip);
-                        break;
-                    case 5:
-                        System.out.println("enter new phone number");
-                        String phoneNumber = input.nextLine();
-                        personToEdit.setPhoneNumber(phoneNumber);
-                        break;
-                    case 6:
-                        quit = true;
-                        break;
-                }
-
-            } while (!quit);
-
+    public void addPerson() {
+        System.out.println("enter first name");
+        String firstName = input.nextLine();
+        System.out.println("enter last name");
+        String lastName = input.nextLine();
+        System.out.println("enter address");
+        String address = input.nextLine();
+        System.out.println("enter city");
+        String city = input.nextLine();
+        System.out.println("enter state");
+        String state = input.nextLine();
+        System.out.println("enter zip");
+        String zip = input.nextLine();
+        System.out.println("enter phone number");
+        String phoneNumber = input.nextLine();
+        Person person1 = new Person(firstName, lastName, address, city, state, zip, phoneNumber);
+        personsList.add(person1);
+        Person duplicate = getObjectWithName(firstName);
+        if (person1.equals(duplicate)) {
+            System.out.println("there already exists person  with same name in address book");
+            return;
         }
+        personsList.add(person1);
+    }
 
-        public Person getObjectWithName(String firstName)
-        {
-            for(Person person:personsList)
-            {
-                if(person.getFirstName().equals(firstName))
-                {
-                    return person;
-                }
+    public void editperson() {
+        System.out.println("enter first name of person ");
+        String firstName = input.nextLine();
+        Person personToEdit = getObjectWithName(firstName);
+        boolean quit = false;
+        do {
+            System.out.println("enter 1 for editing address");
+            System.out.println("enter 2 for editing city");
+            System.out.println("enter 3 for editing state");
+            System.out.println("enter 4 for editing zip");
+            System.out.println("enter 5 for editing phone number");
+            System.out.println("enter 6 to quit");
+            int choice = input.nextInt();
+            input.nextLine();
+            switch (choice) {
+                case 1:
+                    System.out.println("enter new address");
+                    String address = input.nextLine();
+                    personToEdit.setAddress(address);
+                    break;
+                case 2:
+                    System.out.println("enter new city");
+                    String city = input.nextLine();
+                    personToEdit.setCity(city);
+                    break;
+                case 3:
+                    System.out.println("enter new state");
+                    String state = input.nextLine();
+                    personToEdit.setState(state);
+                    break;
+                case 4:
+                    System.out.println("enter new zip");
+                    String zip = input.nextLine();
+                    personToEdit.setZip(zip);
+                    break;
+                case 5:
+                    System.out.println("enter new phone number");
+                    String phoneNumber = input.nextLine();
+                    personToEdit.setPhoneNumber(phoneNumber);
+                    break;
+                case 6:
+                    quit = true;
+                    break;
             }
-            return new Person("","","","","","","");
-        }
 
-        public void deletePerson()
-        {
-            System.out.println("enter first name of person ");
-            String firstName = input.nextLine();
-            Person personToDelete = getObjectWithName(firstName);
-            personsList.remove(personToDelete);
+        } while (!quit);
+
+    }
+
+    public Person getObjectWithName(String firstName) {
+        for (Person person : personsList) {
+            if (person.getFirstName().equals(firstName)) {
+                return person;
+            }
         }
+        return new Person("", "", "", "", "", "", "");
+    }
+
+    public void deletePerson() {
+        System.out.println("enter first name of person ");
+        String firstName = input.nextLine();
+        Person personToDelete = getObjectWithName(firstName);
+        personsList.remove(personToDelete);
     }
 }

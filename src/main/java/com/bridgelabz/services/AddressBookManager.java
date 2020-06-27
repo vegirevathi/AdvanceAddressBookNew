717224c95892bd937433e47d4c03a329178d975a
vegirevathi <vegirevathi@gmail.com>
2020-06-27 16:33:09 +0530

    MODIFIED: Package Structure

--- src/main/java/com/bridgelabz/services/AddressBookManager.java
+++ src/main/java/com/bridgelabz/services/AddressBookManager.java
@@ -1,81 +1,15 @@
 package com.bridgelabz.services;
 
 import com.bridgelabz.model.Person;
-
 import java.util.*;
 
-public class AddressBookMain {
+public class AddressBookManager implements IAddressBookManager {
     static Scanner input = new Scanner(System.in);
-    LinkedList<Person> personsList=new LinkedList<>();
+    public LinkedList<Person> personsList=new LinkedList<>();
     private final HashMap<String,ArrayList<Person>>cityMap = new HashMap<>();
     private final HashMap<String,ArrayList<Person>>stateMap = new HashMap<>();
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Advanced Address Book");
-        boolean quit = false;
-        AddressBookMain addressBook = new AddressBookMain();
-        do{
-            System.out.println("enter 1 for adding person to address book");
-            System.out.println("enter 2 for editing person");
-            System.out.println("enter 3 for deleting a person");
-            System.out.println("enter 4 to display Address book contacts");
-            System.out.println("enter 5 to sort address book contacts by name");
-            System.out.println("enter 6 to sort address book contacts by city");
-            System.out.println("enter 7 to sort address book contacts by state");
-            System.out.println("enter 8 to sort address book contacts by zip");
-            System.out.println("enter 9 to view person by city");
-            System.out.println("enter 10 to view person by state");
-            System.out.println("enter 11 to quit");
-            int option=input.nextInt();
-            input.nextLine();
-            switch (option) {
-                case 1:
-                    addressBook.addPerson();
-                    System.out.println("Contact is added");
-                    break;
-                case 2:
-                    addressBook.displayAddressBook();
-                    addressBook.editPerson();
-                    System.out.println("Contact is edited");
-                    break;
-                case 3:
-                    addressBook.displayAddressBook();
-                    addressBook.deletePerson();
-                    System.out.println("Contact is deleted");
-                    break;
-                case 4:
-                    addressBook.displayAddressBook();
-                    System.out.println("All contacts are shown");
-                    break;
-                case 5:
-                    Collections.sort(addressBook.personsList);
-                    break;
-                case 6:
-                    Collections.sort(addressBook.personsList, Comparator.comparing(person -> person.getCity()));
-                    break;
-                case 7:
-                    Collections.sort(addressBook.personsList, Comparator.comparing(person -> person.getState()));
-                    break;
-                case 8:
-                    Collections.sort(addressBook.personsList, Comparator.comparing(person -> person.getZip()));
-                    break;
-                case 9:
-                    addressBook.viewPersonByCity();
-                    break;
-                case 10:
-                    addressBook.viewPersonByState();
-                    break;
-                case 11:
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("Select anything before you proceed");
-                    break;
-            }
-        }while (!quit);
-    }
-
-    private void displayAddressBook() {
+    public void displayAddressBook() {
         personsList.forEach(x -> {
             System.out.println(x);
         });
---
+++ src/main/java/com/bridgelabz/services/IAddressBookManager.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.services;
+
+import com.bridgelabz.model.Person;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public interface IAddressBookManager {
+    void displayAddressBook();
+    void addPerson();
+    void editPerson();
+    Person getObjectWithName(String firstName);
+    Person getObjectWithPhoneNumber(String phoneNumber);
+    void deletePerson();
+    void viewPersonByCity();
+    void viewPersonByState();
+    void addValuesCity(HashMap<String, ArrayList<Person>> cityMap, String key, Person person);
+    void displayValuesCity(HashMap<String, ArrayList<Person>> cityMap, String city);
+    void addValuesState(HashMap<String,ArrayList<Person>> stateMap,String key,Person person);
+    void displayValuesState(HashMap<String,ArrayList<Person>> stateMap,String state);
+}

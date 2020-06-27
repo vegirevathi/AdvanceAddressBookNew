5912912463333119feb3372d6a95aa52741bb0f8
vegirevathi <vegirevathi@gmail.com>
2020-06-27 08:13:37 +0530

    Displaying Welcome to AddressBook

---
+++ src/main/java/com/bridgelabz/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class AddressBookMain {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to Advanced Address Book");
+    }
+}
